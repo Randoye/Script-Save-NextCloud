b55e54e0df8525655efe97310748491dc4e6974a
Quentin <quentin.julien20@gmail.com>
2021-05-05 09:39:25 +0200

    🚀 Inistialisation

---
+++ Save-NextCloud.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+echo ""
+echo "Save nextcloud 1.0"
+echo "By Randoye"
+echo ""
+#### Préparation des variables
+filenamesave="save-$(date -I)"
+filenamedb="savedb-$(date -I).sql"
+sourcepathfile="/var/www/html/nextcloud/data/Admin/files"
+sourcepathdb="$(pwd)$filenamedb"
+destpathfile="sauvegarde/files/$filenamesave"
+destpathdb="sauvegarde/files/$filenamedb"
+#### Debut du script
+echo "Starting the save script"
+echo ""
+rclone copy $sourcepathfile save:$destpathfile
+echo "The transfert of NextCloud Files has been done !"
+echo ""
+echo "Start the save function for the database"
+echo ""
+mysqldump --routines -u root -p Nextcloud > $filenamedb #Permet d'exporter toutes la base de données fonctions inclus
+rclone copy $filenamedb save:$destpathdb # utilise Rclone pour envoyer la sauvegarde dans le dossier "db"
